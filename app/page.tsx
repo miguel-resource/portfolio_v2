49c0f14cc06339389c8ea53a48c277d9ef12dd83
Miguel Bermúdez Cruz <miguelbermudezcruz@Mac-mini-de-Miguel.local>
2023-08-09 08:37:20 -0600

    [ADD] hardskills

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,35 @@
 "use client";
 import Image from "next/image";
-import { Button } from "@mui/material";
 import s from "./app.module.scss";
+import gsap from "gsap";
 import { Swiper, SwiperSlide } from "swiper/react";
-import SwiperCore, { Navigation } from "swiper/modules";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { use, useEffect, useRef } from "react";
+import SlideHardSkills from "./../components/SlideHardSkills";
+import SlideSoftSkills from "./../components/SlideSoftSkills";
+import { QueryBuilder } from "@mui/icons-material";
+
 
 export default function Home() {
+  const summaryRef = useRef(null);
+
+  useEffect(() => {
+    // const summaryElement: any = summaryRef.current;
+    // const originalText = summaryElement.textContent;
+    // summaryElement.textContent = "";
+
+    // gsap.to(summaryElement, {
+    //   duration: 1,
+    //   text: {
+    //     value: originalText,
+    //   },
+    //   ease: "power3.out",
+    // });
+  }, []);
+
   return (
     <div className={s.home}>
       <div className={s.home__info}>
@@ -20,19 +43,30 @@ export default function Home() {
         />
         <h1>Miguel Ángel Bermúdez Cruz</h1>
         <h3>FullStack Developer</h3>
-        <p>
+        <p className={s.home__info__description} ref={summaryRef}>
           Experienced Software Engineer with a strong focus on frontend
-          development and additional backend capabilities for <b>FullStack</b> work.
-          Proficient in Windows, macOS, and Linux operating systems.
+          development and additional backend capabilities for <b>FullStack</b>{" "}
+          work. Proficient in Windows, macOS, and Linux operating systems.
           Enthusiastic about learning and collaborating within diverse teams.
           Currently, expanding skills in mobile apps and augmented reality,
           while maintaining excellence in web development.
         </p>
 
         <section className={s.home__info__swiper}>
-          <Swiper navigation={true} modules={[Navigation]}>
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
+          <Swiper
+            autoplay={{
+              delay: 5000,
+
+            }}
+            className={s.home__info__swiper__container}
+            modules={[Pagination]} // add autoplay
+          >
+            <SwiperSlide className={s.home__info__swiper__container__slide}>
+              <SlideHardSkills />
+            </SwiperSlide>
+            <SwiperSlide className={s.home__info__swiper__container__slide}>
+              <SlideSoftSkills/>
+            </SwiperSlide>
           </Swiper>
         </section>
       </div>
---
+++ components/SlideHardSkills.tsx
@@ -0,0 +1,113 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Tooltip, TooltipProps, styled, tooltipClasses } from "@mui/material";
+import {
+  faReact,
+  faAngular,
+  faPython,
+  faAws,
+} from "@fortawesome/free-brands-svg-icons";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
+import gsap from "gsap";
+import { useEffect } from "react";
+import s from "./components.module.scss";
+
+const SlideHardSkills = () => {
+  const listHardSkills = [
+    {
+      name: "React",
+      icon: faReact,
+      details:
+        "Typescript, NextJS, Redux Toolkit, Material UI, Tailwind CSS, Styled Components",
+    },
+    {
+      name: "Angular",
+      icon: faAngular,
+      details: "Typescript, Angular Material, Tailwind CSS, Styled Components",
+    },
+    {
+      name: "Python",
+      icon: faPython,
+      details: "Flask, Chalice, Unit Testing, SqlAlchemy",
+    },
+    {
+      name: "AWS",
+      icon: faAws,
+      details: "CloudWatch, S3, EC2",
+    },
+    {
+      name: "DataBase",
+      icon: faDatabase,
+      details: "PostgreSQL, MySQL, Firebase",
+    },
+  ];
+
+  const BootstrapTooltip = styled(({ className, ...props }: TooltipProps) => (
+    <Tooltip {...props} arrow classes={{ popper: className }} />
+  ))(({ theme }) => ({
+    [`& .${tooltipClasses.arrow}`]: {
+      color: "white",
+    },
+    [`& .${tooltipClasses.tooltip}`]: {
+      backgroundColor: "white",
+      color: "black",
+      fontSize: 12,
+      textAlign: "center",
+      paddingX: 1,
+    },
+  }));
+
+  useEffect(() => {
+    let itemHardSkill = document.querySelectorAll(`.${s.hard__skills} li`);
+    let itemP = document.querySelectorAll(`.${s.hard__skills} ${s.name}`);
+
+    gsap.fromTo(
+      itemHardSkill,
+      {
+        opacity: 0,
+        y: -100,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        ease: "power3.out",
+        stagger: 0.2,
+      }
+    );
+    gsap.fromTo(
+        itemP,
+        {
+            opacity: 0,
+        },
+        {
+            opacity: 1,
+            y: 0,
+            display: "block",
+            duration: 1,
+            ease: "power3.out",
+            stagger: 0.2,
+        }
+        );
+  }, []);
+
+  return (
+    <div className={s.hard__skills}>
+      <ul>
+        {listHardSkills.map((item, index) => {
+          return (
+            <li key={index}>
+              <BootstrapTooltip title={item.details} placement="top">
+                <div>
+                  <FontAwesomeIcon icon={item.icon} className={s.icon} />
+                  <p className={s.name}>{item.name}</p>
+                </div>
+              </BootstrapTooltip>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default SlideHardSkills;
