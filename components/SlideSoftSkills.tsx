518ed2d5a6b84ff3908fe5a655d38029542adeb4
Miguel Bermúdez Cruz <miguelbermudezcruz@Mac-mini-de-Miguel.local>
2023-08-14 09:51:22 -0600

    [ADD] softskills items

--- components/SlideSoftSkills.tsx
+++ components/SlideSoftSkills.tsx
@@ -1,58 +1,29 @@
+import { useEffect } from 'react';
+import s from './components.module.scss';
+
+
 const SlideSoftSkills = () => {
+  const listSoftSkills = [
+    'Comunicación',
+    'Iniciativa',
+    'Adaptabilidad',
+    'Pensamiento crítico',
+    'Creatividad'
+  ];
+
+  useEffect(() => {
+    console.log('SlideSoftSkills');
+  }, []);
+
+
   return (
-    <div className="soft-skills">
-      <div className="soft-skills__title">
-        <h3>Soft Skills</h3>
-      </div>
-      <div className="soft-skills__content">
-        <div className="soft-skills__content__item">
-          <div className="soft-skills__content__item__title">
-            <h4>Communication</h4>
-          </div>
-          <div className="soft-skills__content__item__description">
-            <p>
-              I have experience working with people from different countries and
-              cultures. I speak English and Spanish fluently. I am a good
-              listener and I am always open to feedback.
-            </p>
-          </div>
-        </div>
-        <div className="soft-skills__content__item">
-          <div className="soft-skills__content__item__title">
-            <h4>Teamwork</h4>
-          </div>
-          <div className="soft-skills__content__item__description">
-            <p>
-              I have experience working in teams of different sizes. I am a good
-              team player and I am always willing to help others.
-            </p>
-          </div>
-        </div>
-        <div className="soft-skills__content__item">
-          <div className="soft-skills__content__item__title">
-            <h4>Problem Solving</h4>
-          </div>
-          <div className="soft-skills__content__item__description">
-            <p>
-              I am a creative person and I am always looking for new ways to
-              solve problems. I am a fast learner and I am always willing to
-              learn new things.
-            </p>
-          </div>
-        </div>
-        <div className="soft-skills__content__item">
-          <div className="soft-skills__content__item__title">
-            <h4>Time Management</h4>
-          </div>
-          <div className="soft-skills__content__item__description">
-            <p>
-              I am a very organized person and I always try to plan my work
-              ahead of time. I am a fast learner and I am always willing to
-              learn new things.
-            </p>
-          </div>
-        </div>
-      </div>
+    <div className={s.soft__skills}>
+      <ul>
+        {listSoftSkills.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      
+      </ul>
     </div>
   );
 };
